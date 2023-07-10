eb72d28ff1066bbce62c12890b50b924fdcb8696
JT <raikiri24@gmail.com>
2023-07-09 21:09:40 -0400

    update prompts

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
 const mysql = require('mysql2');
 const inquire = require('inquirer');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'pass',
+    database: 'company_db'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    promptMenu();
+});
+
+
+function promptMenu() {
+    inquire.prompt({
+        type: 'list',
+        name: 'reference',
+        message: 'Choose an option',
+        choices: ['Departments', 'Roles', 'Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Exit']
+    }
+    )
+        .then((answers) => {
+            switch (answers.reference) {
+                case 'Departments':
+                    viewDepartments();
+                    break;
+                case 'Roles':
+                    viewRoles();
+                    break;
+                case 'Employees':
+                    viewEmployees();
+                    break;
+                case 'Add Department':
+                    promptAddDepartment();
+                    break;
+                case 'Add Dole':
+                    promptAddRole();
+                    break;
+                case 'Add Employee':
+                    promptAddEmployee();
+                    break;
+                case 'Update Employee Role':
+                    promptUpdateEmployeeRole();
+                    break;
+                case 'Exit':
+                    console.log('Goodbye!');
+                    connection.end();
+                    break;
+            }
+        })
+};
--- schema.sql
+++ schema.sql
@@ -5,21 +5,21 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department(
-    id: INT PRIMARY KEY,
-    name: VARCHAR(30)
+    id INT PRIMARY KEY,
+    name VARCHAR(30)
 );
 
 CREATE TABLE role(
-    id: INT PRIMARY KEY,
-    titel:VARCHAR(30) NOT NULL,
-    salary: DECIMAL NOT NULL,
-    department_id:INT NOT NULL
+    id INT PRIMARY KEY,
+    titel VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
 );
 
 CREATE TABLE employee(
-    id: INT PRIMARY KEY,
-    first_name: VARCHAR(30) NOT NULL,
-    last_name:VARCHAR(30) NOT NULL,
-    role_id: INT NOT NULL,
-    manager_id:INT NOT NULL
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
 );
